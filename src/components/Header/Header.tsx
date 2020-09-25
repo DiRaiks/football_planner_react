f5915026cbbcc9f57d868e0bb26c919ed0fc9dd7
Rustam <rust.ukit@gmail.com>
2020-09-25 12:42:08 +0300

    edit marks, added events count

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { useObserver } from 'mobx-react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { UserStore } from 'store';
-import { ROUTE_EVENTS } from 'constantsVars';
 import { ContainerSticky } from 'reusableComponents';
 
 import Logo from 'components/Logo';
@@ -13,18 +12,13 @@ import HeaderBreadcrumbs from './HeaderBreadcrumbs';
 import $ from './header.module.scss';
 
 const Header: React.FC = () => {
-  const { pathname } = useLocation();
   const userName = useObserver(() => UserStore.user?.name);
   const eventsCount = useObserver(() => UserStore.userEventsCount);
-
-  const onLogoClick = (event: React.MouseEvent): void => {
-    if (pathname === ROUTE_EVENTS) event.preventDefault();
-  };
 
   return (
     <ContainerSticky className={$.container}>
       <div className={$.header}>
-        <Link to="/" className={$.logoLink} onClick={onLogoClick}>
+        <Link to="/events" className={$.logoLink}>
           <Logo />
         </Link>
         <HeaderBreadcrumbs />
